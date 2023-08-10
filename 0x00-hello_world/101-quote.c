1aba41ed36e561a5911117642969d6061934f3a0
Mahmoud shaaban <56820354+mahmoudzin@users.noreply.github.com>
2023-08-10 22:54:34 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,6 +5,7 @@
  * Description: 'the program's description'
  * Return: Always 1(Success)
  */
+
 int main(void)
 {
     fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout)
