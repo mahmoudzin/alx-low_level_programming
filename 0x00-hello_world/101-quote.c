de87aa7bd226d1fe0687682ba02cc32fc8266fd4
Mahmoud shaaban <56820354+mahmoudzin@users.noreply.github.com>
2023-08-10 22:42:51 +0300

    Create 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    char a_static[] = {"a", "n", "d", " ", "t", "h", "a", "t", " ", "p","i","e","c","e", " ", "o", "f", " ", "a","r","t", " ", "i","s", " ", "u","s","e","f","u","l","\"", " ","-", " ", "D","o","r","a", " ", 
+    "K", "o","r","p","a","r",",", "2","0","1","5","-","1","0","-","1","9"};
+  
+    // Write the Character to stdout
+    for (ch = 1; ch <= 57; ch++)
+        putchar(ch);
+  
+    return (1);
+}
