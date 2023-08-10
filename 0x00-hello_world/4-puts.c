c527657f33d1e6dd27c03646e9e916e672de206f
Mahmoud shaaban <56820354+mahmoudzin@users.noreply.github.com>
2023-08-10 20:49:16 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,11 +4,9 @@
  * main - Entry point
  * 
  * Description: 'the program's description'
- * @parameter: describe the parameter
  * 
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
