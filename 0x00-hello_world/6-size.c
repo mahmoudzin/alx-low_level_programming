030bb48f0f0d7ca56fa7c91ae2bd7452a02949e0
Mahmoud shaaban <56820354+mahmoudzin@users.noreply.github.com>
2023-08-10 21:28:49 +0300

    Create 6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Size of a char:", sizeof(char));
+	printf("byte(s)\n");
+	printf("Size of an int::", sizeof(int));
+	printf("byte(s)\n");
+	printf("Size of a long int:", sizeof(long int));
+	printf("byte(s)\n");
+	printf("Size of a float:", sizeof(float));
+	printf("byte(s)");
+}
