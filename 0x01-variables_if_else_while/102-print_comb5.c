fb174d1e063c75454e93af5b7260817816304e2e
Mahmoud shaaban <56820354+mahmoudzin@users.noreply.github.com>
2023-08-12 23:16:05 +0300

    Create 102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - Entry point
+* Description: 'the program's description'
+* Return: Always 1(Success)
+*/
+int main(void)
+{
+int num1 = 0, num2 = 1;
+while (num1 <= 99)
+{
+while (num2 <= 99)
+{
+putchar((num1 / 10) + '0');
+putchar((num1 % 10) + '0');
+putchar(' ');
+putchar((num2 / 10) + '0');
+putchar((num2 % 10) + '0');
+if (num1 != 99 || num2 != 99)
+{
+putchar(',');
+putchar(' ');
+}
+num2++;
+}
+num1++;
+num2 = num1 + 1;
+}
+putchar('\n');
+return 0;
+}
