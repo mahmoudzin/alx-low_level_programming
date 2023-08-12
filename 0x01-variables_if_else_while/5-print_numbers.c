4e8f9333e6b777a1a33f5174085823ee6360ed84
Mahmoud shaaban <56820354+mahmoudzin@users.noreply.github.com>
2023-08-12 20:05:24 +0300

    Create 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 1(Success)
+ */
+int main(void)
+{
+int number = 0;
+while (number < 10)
+{
+printf("%d",number);
+number++;
+}
+putchar('\n');
+return (0);
+}
