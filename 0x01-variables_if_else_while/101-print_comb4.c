3cdae55fd50c0f2bea2699e48bc90aaee0d35009
Mahmoud shaaban <56820354+mahmoudzin@users.noreply.github.com>
2023-08-12 23:04:05 +0300

    Create 101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 1(Success)
+ */
+int main(void)
+{
+int i = 0;
+int j;
+int k;
+while (i <= 10)
+{
+j = i + 1;
+while (j < 10)
+{
+k = j + 1;
+while (k < 10)
+{
+putchar('0' + i);
+putchar('0' + j);
+putchar('0' + k);
+if(i != 7)
+{
+putchar(',');
+putchar(' ');
+}
+k++;
+}
+j++;
+}
+i++;
+}
+putchar('\n');
+return (0);
+}
