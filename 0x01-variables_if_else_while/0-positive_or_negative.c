c698dedd7654a39e3c891dc453073428c340c1cb
mahmoudzin <ms90807040@gmail.com>
2023-08-12 07:33:47 -0700

    add varibles if else while folder

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,26 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <time.h> 
+/**  
+ * main - Entry point  
+ * Description: 'the program's description'  
+ * Return: Always 1(Success)  
+ */
+
+
+int main(void) 
+{
+	int n;
+
+	srand(time(0));  
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)             
+		printf("is positive");     
+	else if (n < 0)
+   		printf("is negative");
+   	else
+   		printf("is zero");
+      
+	return (0);
+}
