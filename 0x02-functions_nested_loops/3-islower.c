20cafdf4bc868c5b6e4da33e631a068866d271e5
Mahmoud shaaban <56820354+mahmoudzin@users.noreply.github.com>
2023-08-16 23:30:15 +0300

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,11 +1,16 @@
 #include "main.h"
+
 /**
- * _islower - Check if Litter is Small ot upper.
+ * _islower - check if char is lowercase
+ * @c: is the char to be checked
+ * Return: 1 if char is lowercase, otherwise 0.
  */
+
 int _islower(int c)
 {
-if (islower(c) > 0)
-return (1);
-else 
-return (0);
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
 }
+
