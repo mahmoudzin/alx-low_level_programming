6b96d79ec3bb4797d4c82d409964c554cee7e6e1
Mahmoud shaaban <56820354+mahmoudzin@users.noreply.github.com>
2023-08-16 23:08:10 +0300

    Create 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Print all letters of English Alphabet ten times.
+ */
+void print_alphabet_x10(void) 
+{
+int i=0;
+while(i < 10)
+{
+char ch = 'a';
+while (ch <= 'z')
+{
+putchar(ch);
+ch++;
+}
+putchar('\n');
+i++;
+}
+}
