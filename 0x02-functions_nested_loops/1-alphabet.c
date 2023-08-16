895943afe8d2873fa6ba9d20e2baa787a777eae4
Mahmoud shaaban <56820354+mahmoudzin@users.noreply.github.com>
2023-08-16 22:34:47 +0300

    Create 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+//#include "main.h"
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0(Success)
+ */
+void print_alphabet(void);
+
+int main(void)
+{
+print_alphabet();
+return (0);
+}
+
+void print_alphabet(void)
+{
+char ch = 'a';
+while (ch <= 'z')
+{
+putchar(ch);
+ch++;
+}
+putchar('\n');
+}
