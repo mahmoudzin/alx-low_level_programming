1aef363a7bcc3ac2e2e26676a68106c8597c51e7
Mahmoud shaaban <56820354+mahmoudzin@users.noreply.github.com>
2023-08-17 22:23:44 +0300

    Create 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - multiplies two integers
+ * @a: first int
+ * @b: second int
+ * Return: int 1 if uppercase, 0 otherwies
+ */
+int mul(int a, int b)
+{
+int result = a * b;
+return (result);
+}
