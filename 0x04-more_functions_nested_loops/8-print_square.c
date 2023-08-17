902715441d689c502de4f790bb8fe2462a2a44e0
Mahmoud shaaban <56820354+mahmoudzin@users.noreply.github.com>
2023-08-17 23:53:02 +0300

    Create 8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _isupper - check if litter is uppercase
+ * @c: int c to load the litter
+ * Return: int 1 if uppercase, 0 otherwies
+ */
+void print_square(int size)
+{
+int i, j;
+if (n <= 0)
+_putchar('\n');
+else
+{
+for (i = 0; i < n; i++)
+{
+for (j = 0; j < n; j++)
+_putchar('#');
+_putchar('\n');
+}      
+}
+}
