d406d46769eae1599cb50773a3e0df38316731c8
Mahmoud shaaban <56820354+mahmoudzin@users.noreply.github.com>
2023-08-17 23:22:23 +0300

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,10 +6,11 @@
  */
 void print_line(int n)
 {
+int i;
 if (n <= 0)
-_putchar('\n');
+  _putchar('\n');
 else
-for (int i = 0; i < n; i++)
-_putchar('_');
-_putchar('\n');
+  for (i = 0; i < n; i++)
+    _putchar('_');
+  _putchar('\n');
 }
