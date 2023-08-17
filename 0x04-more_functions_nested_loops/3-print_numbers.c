5a3967712100b98949c20f1141fe88cc2ecd55be
Mahmoud shaaban <56820354+mahmoudzin@users.noreply.github.com>
2023-08-17 22:31:48 +0300

    Create 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - print Number form 0 to 9
+ * Return: void
+ */
+void print_numbers(void)
+{
+int i=0;
+while (i < 10)
+{
+_putchar('0' + i);
+i++;
+}
+_putchar('\n');
+}
