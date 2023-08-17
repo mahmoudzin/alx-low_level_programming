97280563c636d92775cf2de4f3ed4dd887cafe25
Mahmoud shaaban <56820354+mahmoudzin@users.noreply.github.com>
2023-08-17 22:59:36 +0300

    Create 5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * more_numbers - print more numbers
+ * Return: void
+ */
+void more_numbers(void)
+{
+int i = 0; 
+int j=0;
+while (j < 10)
+{
+i = 0;
+while (i <= 14)
+{
+if (i < 10)
+_putchar('0' + i);
+else {
+_putchar('1');
+_putchar('0' + (i % 10));
+}
+i++;
+}
+_putchar('\n');
+j++; 
+}
+_putchar('\n');
+}
