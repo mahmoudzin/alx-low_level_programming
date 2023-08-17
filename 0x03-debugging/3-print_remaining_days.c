fdb5b5dcd12a7ee510e26e7baa4be83fcc9c3bf4
Mahmoud shaaban <56820354+mahmoudzin@users.noreply.github.com>
2023-08-17 16:34:06 +0300

    Update 3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,10 +12,6 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-/**
-* the year 
-* year 400
-*/
 if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
 {
 if (month >= 2 && day >= 60)
