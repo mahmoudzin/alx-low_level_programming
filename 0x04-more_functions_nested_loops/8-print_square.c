08c071a90473408e4671c2aab3da5429a557a8b5
Mahmoud shaaban <56820354+mahmoudzin@users.noreply.github.com>
2023-08-18 00:16:52 +0300

    Update 8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
- * _isupper - check if litter is uppercase
- * @c: int c to load the litter
- * Return: int 1 if uppercase, 0 otherwies
+ * print_square - print square
+ * @size: int size the number of rows and columns
+ * Return: void
  */
 void print_square(int size)
 {
 int i, j;
-for (i = 0; i < n; i++)
+for (i = 0; i < size; i++)
 {
-for (j = 0; j < n; j++)
+for (j = 0; j < size; j++)
 _putchar(35);
 _putchar('\n');
 }
