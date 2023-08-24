13113798b0048e2427095dc7b3e1f57418450a7f
Mahmoud shaaban <56820354+mahmoudzin@users.noreply.github.com>
2023-08-24 21:58:19 +0300

    Create _strncat.c

---
+++ 0x06-pointers_arrays_strings/_strncat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _isupper - check if litter is uppercase
+ * @dest: first string
+ * @src: second string
+ * @n: the number
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int c, c2;
+c = 0;
+while (dest[c])
+c++;
+for (c2 = 0; n && src[c2] != '\0'; c2++)
+{
+dest[c++] = src[c2];
+n--;
+}
+dest[c + c2] = '\0';
+return (dest);
+}
