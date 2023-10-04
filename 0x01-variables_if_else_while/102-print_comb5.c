6d569f40e0b972121a5bfd3166819db436834e96
Mahmoud shaaban <56820354+mahmoudzin@users.noreply.github.com>
2023-10-04 13:04:43 +0300

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,16 +13,13 @@ while (num1 <= 99)
 {
 while (num2 <= 99)
 {
-putchar((num1 / 10) + '0');
-putchar((num1 % 10) + '0');
+putchar(num1 / 10 + '0');
+putchar(num1 % 10 + '0');
 putchar(' ');
-putchar((num2 / 10) + '0');
-putchar((num2 % 10) + '0');
-if (num1 != 99 || num2 != 99)
-{
+putchar(num2 / 10 + '0');
+putchar(num2 % 10 + '0');
 putchar(',');
 putchar(' ');
-}
 num2++;
 }
 num1++;
